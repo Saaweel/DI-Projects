c62a86f2ed12cfed3a95f7824cfe29008e4efc72
Saaweel <sawelsports@gmail.com>
2023-09-22 21:00:57 +0200

    Finished

--- GestorDeContactos/src/Contactos.java
+++ GestorDeContactos/src/Contactos.java
@@ -1,7 +1,76 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
 
 public class Contactos {
     private JPanel window;
+    private JTextField nameInput;
+    private JTextField emailInput;
+    private JTable contactsTable;
+    DefaultTableModel tableModel = new DefaultTableModel();
+    private JButton agregarButton;
+    private JButton editarButton;
+    private JButton eliminarButton;
+
+    public Contactos() {
+        contactsTable.setModel(tableModel);
+        tableModel.addColumn("Nombre");
+        tableModel.addColumn("Email");
+        agregarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = nameInput.getText();
+                String email = emailInput.getText();
+
+                if (!name.isEmpty() && !email.isEmpty()) {
+                    Vector<String> v = new Vector<>();
+                    v.add(name);
+                    v.add(email);
+                    tableModel.addRow(v);
+                    nameInput.setText("");
+                    emailInput.setText("");
+                } else {
+                    JOptionPane.showMessageDialog(window, "Debes introducir un nombre y email");
+                }
+            }
+        });
+        editarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selected = contactsTable.getSelectedRow();
+
+                if (selected != -1) {
+                    String name = nameInput.getText();
+                    String email = emailInput.getText();
+
+                    if (!name.isEmpty() && !email.isEmpty()) {
+                        tableModel.setValueAt(name, selected, 0);
+                        tableModel.setValueAt(email, selected, 1);
+                        nameInput.setText("");
+                        emailInput.setText("");
+                    } else {
+                        JOptionPane.showMessageDialog(window, "Debes introducir un nombre y email");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(window, "Debes seleccionar un contacto");
+                }
+            }
+        });
+        eliminarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selected = contactsTable.getSelectedRow();
+
+                if (selected != -1) {
+                    tableModel.removeRow(selected);
+                } else {
+                    JOptionPane.showMessageDialog(window, "Debes seleccionar un contacto");
+                }
+            }
+        });
+    }
 
     public JPanel getWindow() {
         return window;
--- GestorDeContactos/src/Main.java
+++ GestorDeContactos/src/Main.java
@@ -2,9 +2,11 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Calculadora");
+        JFrame frame = new JFrame("Contactos");
 
         frame.getContentPane().add(new Contactos().getWindow());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
         frame.setSize(400, 400);
 
         frame.setVisible(true);
