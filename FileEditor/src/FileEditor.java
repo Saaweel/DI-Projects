8ccf30c298a70508a85014cf2209fad99a76a2a0
Saaweel <sawelsports@gmail.com>
2023-09-28 16:18:05 +0200

    Update FileEditor.java

--- FileEditor/src/FileEditor.java
+++ FileEditor/src/FileEditor.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class FileEditor {
     private JPanel window;
     private JTextArea textArea;
 
-    public FileEditor(JFrame jframe) {
+    public FileEditor(JFrame frame) {
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("Archivo");
 
@@ -16,7 +22,45 @@ public class FileEditor {
 
         menuBar.add(menu);
 
-        jframe.setJMenuBar(menuBar);
+        frame.setJMenuBar(menuBar);
+
+        openFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int result = fileChooser.showOpenDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    try {
+                        textArea.setText("");
+                        Scanner scanner = new Scanner(selectedFile);
+                        while (scanner.hasNextLine()) {
+                            textArea.append(scanner.nextLine() + "\n");
+                        }
+                        scanner.close();
+                    } catch (Exception ex) {
+                        ex.fillInStackTrace();
+                    }
+                }
+            }
+        });
+        saveFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int result = fileChooser.showOpenDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    try {
+                        FileWriter fileWriter = new FileWriter(selectedFile);
+                        fileWriter.write(textArea.getText());
+                        fileWriter.close();
+                    } catch (Exception ex) {
+                        ex.fillInStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     public JPanel getWindow() {
