ed8093f78a1625aa13c125c389a9393d3c0fc0d7
Saaweel <sawelsports@gmail.com>
2023-10-17 19:54:10 +0200

    .

--- LecturaEscritura/src/main/java/com/saaweel/PrimaryController.java
+++ LecturaEscritura/src/main/java/com/saaweel/PrimaryController.java
@@ -1,8 +1,52 @@
 package com.saaweel;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class PrimaryController {
+    public BorderPane window;
+    public TextArea textViewer;
+    public void handleClearButton() {
+        textViewer.clear();
+    }
 
+    public void openDocument() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Selecciona un archivo");
+        File file = fileChooser.showOpenDialog(window.getScene().getWindow());
+
+        if (file != null && file.exists() && file.isFile()) {
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    textViewer.appendText(line + "\n");
+                }
+            } catch (Exception e) {
+                e.fillInStackTrace();
+            }
+        }
+    }
+
+    public void saveDocument() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Selecciona un archivo");
+        File file = fileChooser.showOpenDialog(window.getScene().getWindow());
+
+        if (file != null && file.exists() && file.isFile()) {
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+                Scanner textAreaReader = new Scanner(textViewer.getText());
+
+                while (textAreaReader.hasNextLine()) {
+                    bw.write(textAreaReader.nextLine() + "\n");
+                }
+            } catch (Exception e) {
+                e.fillInStackTrace();
+            }
+        }
+    }
 }
