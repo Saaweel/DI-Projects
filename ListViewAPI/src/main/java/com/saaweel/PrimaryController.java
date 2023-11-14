45b1a18b1be8e113497e9d6f2a43d6cede5c5c16
Saaweel <sawelsports@gmail.com>
2023-11-14 18:23:16 +0100

    .

--- ListViewAPI/src/main/java/com/saaweel/PrimaryController.java
+++ ListViewAPI/src/main/java/com/saaweel/PrimaryController.java
@@ -32,21 +32,20 @@ public class PrimaryController {
                     return new ListCell<>() {
                         @Override
                         protected void updateItem(Datum datum, boolean empty) {
-                            super.updateItem(datum, empty);
+                        super.updateItem(datum, empty);
 
-                            if (datum == null || empty) {
-                                setText(null);
-                                setGraphic(null);
-                            } else {
-                                String title = datum.attributes.canonicalTitle;
-                                String posterImage = datum.attributes.posterImage.medium;
-                                String synopsis = datum.attributes.synopsis;
-                                String startDate = datum.attributes.startDate;
-                                String ageRating = datum.attributes.ageRating;
-                                int episodeCount = datum.attributes.episodeCount;
-                                String showType = datum.attributes.showType;
-
-                            }
+                        if (datum == null || empty) {
+                            setText(null);
+                            setGraphic(null);
+                        } else {
+                            String title = datum.attributes.canonicalTitle;
+                            String posterImage = datum.attributes.posterImage.medium;
+                            String synopsis = datum.attributes.synopsis;
+                            String startDate = datum.attributes.startDate;
+                            String ageRating = datum.attributes.ageRating;
+                            int episodeCount = datum.attributes.episodeCount;
+                            String showType = datum.attributes.showType;
+                        }
                         }
                     };
                 }
