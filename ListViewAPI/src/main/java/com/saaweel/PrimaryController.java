af3c7152080b532eaed4b57316d400a1e0e0d67d
Saaweel <sawelsports@gmail.com>
2023-11-15 20:35:57 +0100

    ListViewAPI

--- ListViewAPI/src/main/java/com/saaweel/PrimaryController.java
+++ ListViewAPI/src/main/java/com/saaweel/PrimaryController.java
@@ -3,27 +3,47 @@ package com.saaweel;
 import com.saaweel.apimodels.AnimeData;
 import com.saaweel.apimodels.Datum;
 import javafx.collections.FXCollections;
-import javafx.scene.control.ListCell;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
-import javafx.util.Callback;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class PrimaryController {
+    private KitsuAPIClient kitsuClient;
 
     public ListView<Datum> listView;
 
+    public TextField limitInput;
+
+    public TextField offsetInput;
+
     public void initialize() {
+        kitsuClient = new KitsuAPIClient();
 
-        KitsuAPIClient client = new KitsuAPIClient();
+        listView.setCellFactory(param -> new AnimeFXMLListCell());
+
+        loadAnimes(10, 0);
+    }
+
+    public void search() {
         try {
-            AnimeData animes = client.getAnimes(10, 10);
-            listView.setItems(FXCollections.observableList(animes.data));
+            if (!limitInput.getText().isEmpty() && !offsetInput.getText().isEmpty()) {
+                loadAnimes(Integer.parseInt(limitInput.getText()), Integer.parseInt(offsetInput.getText()));
+            }
+        } catch (NumberFormatException e) {
+            new Alert(Alert.AlertType.ERROR, "Los parámetros introducidos no son válido").show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-            listView.setCellFactory(param -> new AnimeFXMLListCell());
+    public void loadAnimes(int limit, int offset) {
+        try {
+            AnimeData animes = kitsuClient.getAnimes(limit, offset);
+            listView.setItems(FXCollections.observableList(animes.data));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
