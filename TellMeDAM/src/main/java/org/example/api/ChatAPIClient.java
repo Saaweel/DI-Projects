ca6270a521396ae5594f68494c41bae02e2cc543
Saaweel <sawelsports@gmail.com>
2023-12-05 17:18:58 +0100

    Updated API

--- TellMeDAM/src/main/java/org/example/api/ChatAPIClient.java
+++ TellMeDAM/src/main/java/org/example/api/ChatAPIClient.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 public class ChatAPIClient extends RootAPIClient {
-    public void createChat(String userId, String otherUserId, APICallback callback) {
+    public void createChat(Integer userId, Integer otherUserId, APICallback callback) {
         new Thread(() -> {
             try {
                 doCreateChat(userId, otherUserId, callback);
@@ -38,66 +38,19 @@ public class ChatAPIClient extends RootAPIClient {
 
     }
 
-    private void doCreateChat(String userId, String otherUserId, APICallback callback) throws IOException, InterruptedException {
-        String url = BASE_URL + "/chat";
-        String requestBody = "{\n" +
-                "  \"data\": {\n" +
-                "    \"type\": \"chat\",\n" +
-                "    \"attributes\": {},\n" +
-                "    \"relationships\": {\n" +
-                "      \"messages\": {\n" +
-                "        \"data\": []\n" +
-                "      },\n" +
-                "      \"users\": {\n" +
-                "        \"data\": [\n" +
-                "          {\n" +
-                "            \"type\": \"user\",\n" +
-                "            \"id\": \"" + userId + "\"\n" +
-                "          },\n" +
-                "          {\n" +
-                "            \"type\": \"user\",\n" +
-                "            \"id\": \"" + otherUserId + "\"\n" +
-                "          }\n" +
-                "        ]\n" +
-                "      }\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"meta\": {\n" +
-                "    \"serialization\": \"string\",\n" +
-                "    \"additionalProp1\": {}\n" +
-                "  }\n" +
-                "}";
-
-
+    private void doCreateChat(Integer userId, Integer otherUserId, APICallback callback) throws IOException, InterruptedException {
+        String url = BASE_URL + "/chats";
+        String requestBody = "{\"user1_id\":" + userId + ",\"user2_id\":" + otherUserId + "}";
         HttpResponse<String> response = doPOSTRequest(url, requestBody);
 
-        JsonNode jsonNode = new ObjectMapper().readTree(response.body());
-        if (jsonNode.has("error")) {
-            Map<String, String> errorResult = new HashMap<>();
-            errorResult.put("error", jsonNode.get("error").asText());
-            Platform.runLater(() -> callback.onError(errorResult));
+        if (response.statusCode() == 200) {
+            Gson gson = new Gson();
+            Chat chat = gson.fromJson(response.body(), Chat.class);
+            onSuccess(callback, chat);
         } else {
-            JsonNode dataNode = jsonNode.get("data");
-            JsonNode attributesNode = dataNode.get("attributes");
-            JsonNode relationshipsNode = dataNode.get("relationships");
-            Map<String, String> result = new HashMap<>();
-            result.put("id", dataNode.get("id").asText());
-            result.put("createdAt", attributesNode.get("createdAt").asText());
-            result.put("updatedAt", attributesNode.get("updatedAt").asText());
-
-            int index = 0;
-            for (JsonNode user : relationshipsNode.get("users").get("data")) {
-                result.put("user" + index, user.get("id").asText());
-                index++;
-            }
-
-            Platform.runLater(() -> {
-                try {
-                    callback.onSuccess(result);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+            Gson gson = new Gson();
+            Error error = gson.fromJson(response.body(), Error.class);
+            onError(callback, error);
         }
     }
 
