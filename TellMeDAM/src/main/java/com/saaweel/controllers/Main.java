2302e57b113f435d8896f29f339e0f5b25602d5e
Saaweel <sawelsports@gmail.com>
2023-12-09 16:10:03 +0100

    Update Main.java

--- TellMeDAM/src/main/java/com/saaweel/controllers/Main.java
+++ TellMeDAM/src/main/java/com/saaweel/controllers/Main.java
@@ -1,12 +1,20 @@
 package com.saaweel.controllers;
 
 import com.saaweel.App;
+import org.example.api.APICallback;
+import org.example.api.ChatAPIClient;
+import org.example.api.model.Chat;
+import org.example.api.model.Error;
 import org.example.api.model.User;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
+    private ChatAPIClient chatApi;
     public void initialize() {
+        chatApi = new ChatAPIClient();
+
         new Thread(() -> {
             synchronized (App.waitingForSceneLoad) {
                 if (App.getScene() == null) {
@@ -27,12 +35,31 @@ public class Main {
                     throw new RuntimeException(e);
                 }
             } else {
-                loadMainInfo();
+                loadMainInfo(user);
             }
         }).start();
+
+
     }
 
-    private void loadMainInfo() {
-        System.out.println("Cargar información");
+    private void loadMainInfo(User user) {
+        chatApi.getAllChatsFromUser(user.getId(), new APICallback() {
+            @Override
+            public void onSuccess(Object response) {
+                if (response instanceof List) {
+                    List<Chat> chats = (List<Chat>) response;
+
+                    System.out.println(chats);
+                }
+            }
+
+            @Override
+            public void onError(Object response) {
+                if (response instanceof Error) {
+                    Error error = (Error) response;
+                    App.showNotification("Error al cargar los chats", error.getError());
+                }
+            }
+        });
     }
 }
